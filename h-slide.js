d11d6a5f122a6fc095106c618ec138b60a0e41ca
Harold Campbell <harold.campbell@gmail.com>
2017-12-27 14:08:55 +0200

    [feat] 0.0.4 Allow slider updated outside of slider container.
    [chore] add tracking comments

--- h-slide.js
+++ h-slide.js
@@ -1,166 +1,174 @@
 /**
  * Created by: Harold Campbell
  * twitter: @haroldcampbell
- * version 0.0.3
+ * version 0.0.4
  */
+
 hslider = {
-    __createSlider(knobElm, containerElm) {
-        return {
-            isEnabled: false,
-            isInitialize: false,
-            knob: {
-                elm: knobElm,
-                width: knobElm.scrollWidth,
-                position: {
-                    x: 0,
-                    y: 0
-                },
-                range: 0,
-                onRangeChanged: null,
-                startOffset: 0,
+    __activeSlider: null,
+};
+
+hslider.__createSlider = function(knobElm, containerElm, labelElm) {
+    return {
+        isEnabled: false,
+        isInitialize: false,
+        labelElm: labelElm,
+
+        knob: {
+            elm: knobElm,
+            width: knobElm.scrollWidth,
+            position: {
+                x: 0,
+                y: 0
             },
-            container: {
-                elm: containerElm,
-                width: containerElm.scrollWidth,
-                sliderWidth: containerElm.scrollWidth - knobElm.clientWidth,
-            },
-            updateKnobPosition(containerEvent) {
-                let w = this.container.elm.scrollWidth;
-                let left = this.container.elm.style.left;
-                let x = containerEvent.screenX - this.container.elm.offsetLeft;
+            range: 0,
+            onRangeChanged: null,
+            startOffset: 0,
+        },
 
-                this.knob.position.x = x - this.knob.startOffset;
+        container: {
+            elm: containerElm,
+            width: containerElm.scrollWidth,
+            sliderWidth: containerElm.scrollWidth - knobElm.clientWidth,
+        },
 
-                if (x < this.knob.startOffset) {
-                    this.knob.position.x = 0;
-                } else if (this.knob.position.x + this.knob.width > this.container.width) {
-                    this.knob.position.x = this.container.sliderWidth;
-                }
+        updateKnobPosition(documentEvent) {
+            let rect = this.container.elm.getBoundingClientRect()
+            let left = documentEvent.clientX - rect.left;
 
-                this.knob.range = this.knob.position.x / this.container.sliderWidth;
-                if (this.knob.onRangeChanged != null) {
-                    this.knob.onRangeChanged(this);
-                }
+            this.knob.position.x = left - this.knob.startOffset;
 
-                this.knob.elm.style.left = this.knob.position.x;
-            },
-        };
-    },
-
-    __initSlider(event, knobElm) {
-        let containerElm = knobElm.parentNode;
-        let slider = containerElm.slider;
-
-        slider.knob.width = knobElm.scrollWidth
-        slider.knob.startOffset = event.screenX - containerElm.offsetLeft;
-
-        slider.container.width = containerElm.scrollWidth;
-        slider.container.sliderWidth = containerElm.scrollWidth - knobElm.clientWidth;
-        slider.isInitialize = true;
-
-        document.onmouseup = function (event) {
-            /** Used to disable the slider if the user did the mouseup
-             * outside of the container.  */
-            if (slider.isEnabled) {
-                slider.isEnabled = false;
-            }
-        }
-    },
-
-    __setSliderStatus(event, knobElm, isEnabled) {
-        let slider = knobElm.parentNode.slider;
-
-        if (!slider.isInitialize) {
-            this.__initSlider(event, knobElm);
-        }
-
-        slider.isEnabled = isEnabled;
-    },
-
-    knobEnable(event, knobElm) {
-        this.__setSliderStatus(event, knobElm, true)
-    },
-
-    knobDisable(event, knobElm) {
-        this.__setSliderStatus(event, knobElm, false)
-    },
-
-    updateKnobPosition(event, containerElm, labelId) {
-        if (!containerElm.slider.isInitialize || !containerElm.slider.isEnabled) {
-            return;
-        }
-
-        containerElm.slider.updateKnobPosition(event);
-
-        let label = document.getElementById(labelId);
-        if (label != null) {
-            label.innerText = containerElm.slider.knob.range;
-        }
-    },
-
-    /**
-     * Disables the slider if the user does a mouseup
-     * while inside the slider-container
-     */
-    sliderDisable(event, owner, labelId) {
-        if (owner.slider === undefined || owner.slider === null) {
-            return;
-        }
-
-        if (event.relatedTarget == owner.slider.knob.elm) {
-            return;
-        }
-
-        this.updateKnobPosition(event, owner, labelId);
-        owner.slider.isEnabled = false;
-    },
-
-    __textToHTMLNode(text) {
-        let temp = document.createElement('div');
-        temp.innerHTML = text;
-
-        return temp.firstChild;
-    },
-
-    initializeSliders() {
-        let sliders = document.getElementsByTagName("hslider");
-
-        for (let index = 0; index < sliders.length; index++) {
-            let elm = sliders[index];
-            let knobElm = this.__textToHTMLNode('<div id="knob" class="slider-knob"></div>')
-
-            knobElm.onmousedown = function (event) {
-                hslider.knobEnable(event, knobElm);
-            }
-            knobElm.onmouseup = function (event) {
-                hslider.knobDisable(event, knobElm);
+            if (left < this.knob.startOffset) {
+                this.knob.position.x = 0;
+            } else if (this.knob.position.x + this.knob.width > this.container.width) {
+                this.knob.position.x = this.container.sliderWidth;
             }
 
-            let labelId = elm.getAttribute('labelid');
-            let containerElm = this.__textToHTMLNode('<div class="slider-container"></div>');
-
-            containerElm.onmouseout = function (event) {
-                hslider.sliderDisable(event, containerElm, labelId);
-            }
-            containerElm.onmousemove = function (event) {
-                hslider.updateKnobPosition(event, containerElm, labelId);
+            this.knob.range = this.knob.position.x / this.container.sliderWidth;
+            if (this.knob.onRangeChanged != null) {
+                this.knob.onRangeChanged(this);
             }
 
-            containerElm.appendChild(knobElm);
-            elm.appendChild(containerElm);
+            this.knob.elm.style.left = this.knob.position.x;
+        },
 
-            containerElm.slider = this.__createSlider(knobElm, containerElm)
-        }
-    },
+        updateLabelText() {
+            if (this.labelElm != null) {
+                this.labelElm.innerText = this.knob.range;
+            }
+        },
 
-    getSliderById(hsliderId) {
-        let hsliderElm = document.getElementById(hsliderId);
-        if(hsliderElm == null) {
-            return null;
-        }
+        onmouseup(event) {
+            if (!hslider.__hasActiveSlider()) {
+                return;
+            }
 
-        let sliderElm = hsliderElm.querySelectorAll(":scope .slider-container")[0];
+            if (hslider.__activeSlider.isEnabled) {
+                hslider.__activeSlider.isEnabled = false;
+            }
+        },
 
-        return sliderElm.slider;
+        onmousemove(event) {
+            if (!hslider.__hasActiveSlider() || !hslider.__isActive()) {
+                return;
+            }
+
+            hslider.__activeSlider.updateKnobPosition(event);
+            hslider.__activeSlider.updateLabelText();
+        },
+    };
+}
+
+hslider.__hasActiveSlider = function() {
+    return (this.__activeSlider !== undefined && this.__activeSlider !== null);
+}
+
+hslider.__isActive = function(){
+    return this.__activeSlider.isInitialize && hslider.__activeSlider.isEnabled
+}
+
+hslider.__initSlider = function(event, knobElm) {
+    let containerElm = knobElm.parentNode;
+    let slider = containerElm.slider;
+
+    slider.knob.width = knobElm.scrollWidth
+    slider.knob.startOffset = event.screenX - containerElm.offsetLeft;
+
+    slider.container.width = containerElm.scrollWidth;
+    slider.container.sliderWidth = containerElm.scrollWidth - knobElm.clientWidth;
+    slider.isInitialize = true;
+}
+
+hslider.__setSliderStatus = function(event, knobElm, isEnabled) {
+    document.onmouseup = null;
+    document.onmousemove = null;
+
+    let slider = knobElm.parentNode.slider;
+
+    if (!slider.isInitialize) {
+        this.__initSlider(event, knobElm);
     }
+
+    slider.isEnabled = isEnabled;
+    this.__activeSlider = slider;
+
+    if (isEnabled) {
+        document.onmouseup = slider.onmouseup;
+        document.onmousemove = slider.onmousemove;
+    }
+}
+
+hslider.__textToHTMLNode = function(text) {
+    let temp = document.createElement('div');
+    temp.innerHTML = text;
+
+    return temp.firstChild;
+}
+
+hslider.knobEnable = function(event, knobElm) {
+    this.__setSliderStatus(event, knobElm, true)
+}
+
+hslider.knobDisable = function(event, knobElm) {
+    this.__setSliderStatus(event, knobElm, false)
+}
+
+hslider.initializeSliders = function() {
+    let sliders = document.getElementsByTagName("hslider");
+
+    for (let index = 0; index < sliders.length; index++) {
+        let elm = sliders[index];
+        let knobElm = this.__textToHTMLNode('<div id="knob" class="slider-knob"></div>')
+
+        knobElm.onmousedown = function (event) {
+            hslider.knobEnable(event, knobElm);
+        }
+        knobElm.onmouseup = function (event) {
+            hslider.knobDisable(event, knobElm);
+        }
+
+        let labelElm = null;
+        let labelId = elm.getAttribute('labelid');
+        let containerElm = this.__textToHTMLNode('<div class="slider-container"></div>');
+
+        if (labelId != null) {
+            labelElm = document.getElementById(labelId);
+        }
+
+        containerElm.appendChild(knobElm);
+        elm.appendChild(containerElm);
+
+        containerElm.slider = this.__createSlider(knobElm, containerElm, labelElm)
+    }
+}
+
+hslider.getSliderById = function(hsliderId) {
+    let hsliderElm = document.getElementById(hsliderId);
+    if (hsliderElm == null) {
+        return null;
+    }
+
+    let sliderElm = hsliderElm.querySelectorAll(":scope .slider-container")[0];
+
+    return sliderElm.slider;
 }
